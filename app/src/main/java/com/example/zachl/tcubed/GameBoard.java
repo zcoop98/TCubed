e8c0b1699554d909d419789dfff48c80ceb218dd
zcoop98 <zcoop98@yahoo.com>
2018-11-06 22:12:45 -0800

    All java code take2

--- app/src/main/java/com/example/zachl/tcubed/GameBoard.java
+++ app/src/main/java/com/example/zachl/tcubed/GameBoard.java
@@ -1,13 +1,100 @@
 package com.example.zachl.tcubed;
+import java.util.Random;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+public class GameBoard {
 
-public class GameBoard extends AppCompatActivity {
+    private static final Random RANDOM = new Random();
+    private char[] elts;
+    private char currentPlayer;
+    private boolean ended;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_game_board);
+    public GameBoard() {
+        elts = new char[9];
+        newGame();
     }
+
+    public boolean isEnded() {
+        return ended;
+    }
+
+    public char play(int x, int y) {
+        if (!ended  &&  elts[3 * y + x] == ' ') {
+            elts[3 * y + x] = currentPlayer;
+            changePlayer();
+        }
+
+        return checkEnd();
+    }
+
+    public void changePlayer() {
+        currentPlayer = (currentPlayer == 'X' ? 'O' : 'X');
+    }
+
+    public char elt(int x, int y) {
+        return elts[3 * y + x];
+    }
+
+    public void newGame() {
+        for (int i = 0; i  < elts.length; i++) {
+            elts[i] = ' ';
+        }
+
+        currentPlayer = 'X';
+        ended = false;
+    }
+
+    public char checkEnd() {
+        for (int i = 0; i < 3; i++) {
+            if (elt(i, 0) != ' ' &&
+                    elt(i, 0) == elt(i, 1)  &&
+                    elt(i, 1) == elt(i, 2)) {
+                ended = true;
+                return elt(i, 0);
+            }
+
+            if (elt(0, i) != ' ' &&
+                    elt(0, i) == elt(1, i)  &&
+                    elt(1, i) == elt(2, i)) {
+                ended = true;
+                return elt(0, i);
+            }
+        }
+
+        if (elt(0, 0) != ' '  &&
+                elt(0, 0) == elt(1, 1)  &&
+                elt(1, 1) == elt(2, 2)) {
+            ended = true;
+            return elt(0, 0);
+        }
+
+        if (elt(2, 0) != ' '  &&
+                elt(2, 0) == elt(1, 1)  &&
+                elt(1, 1) == elt(0, 2)) {
+            ended = true;
+            return elt(2, 0);
+        }
+
+        for (int i = 0; i < 9; i++) {
+            if (elts[i] == ' ')
+                return ' ';
+        }
+
+        return 'T';
+    }
+
+    public char computer() {
+        if (!ended) {
+            int position = -1;
+
+            do {
+                position = RANDOM.nextInt(9);
+            } while (elts[position] != ' ');
+
+            elts[position] = currentPlayer;
+            changePlayer();
+        }
+
+        return checkEnd();
+    }
+
 }
--- app/src/main/java/com/example/zachl/tcubed/MainActivity.java
+++ app/src/main/java/com/example/zachl/tcubed/MainActivity.java
@@ -1,14 +1,61 @@
 package com.example.zachl.tcubed;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import static com.example.zachl.tcubed.R.id.GameBoard;
 
 public class MainActivity extends AppCompatActivity {
+
+    private BoardView boardView;
+    private GameBoard gameEngine;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boardView = (BoardView) findViewById(GameBoard);
+        gameEngine = new GameBoard();
+        boardView.setGameEngine(gameEngine);
+        boardView.setMainActivity(this);
     }
-    //This is a test to check commits
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_new_game) {
+            newGame();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void gameEnded(char c) {
+        String msg = (c == 'T') ? "Game Ended. Tie" : "GameEnded. " + c + " win";
+
+        new AlertDialog.Builder(this).setTitle("T CUBED").
+                setMessage(msg).
+                setOnDismissListener(new DialogInterface.OnDismissListener() {
+                    @Override
+                    public void onDismiss(DialogInterface dialogInterface) {
+                        newGame();
+                    }
+                }).show();
+    }
+
+    private void newGame() {
+        gameEngine.newGame();
+        boardView.invalidate();
+    }
+
+
 }
