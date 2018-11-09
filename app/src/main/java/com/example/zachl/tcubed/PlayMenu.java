315037854e28d4a82d859efe6f243b2e3fcac376
Jillianm <jillian.e.mcdaniel@bioal.edu>
2018-11-08 21:24:31 -0800

    PlayMenu intents updated

--- app/src/main/java/com/example/zachl/tcubed/PlayMenu.java
+++ app/src/main/java/com/example/zachl/tcubed/PlayMenu.java
@@ -1,12 +1,18 @@
 package com.example.zachl.tcubed;
 
 import android.content.Intent;
+import android.net.UrlQuerySanitizer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Switch;
 
 public class PlayMenu extends AppCompatActivity {
-
+    public static final String XNAME = "com.zachl.tcubed.XNAME";
+    public static final String ONAME = "com.zachl.tcubed.ONAME";
+    public static final String PVPSWITCH = "com.zachl.tcubed.PVPSWITCH";
+    public static final String XOSWITCH = "com.zachl.tcubed.XOSWITCH";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,8 +21,20 @@ public class PlayMenu extends AppCompatActivity {
 
     // Intent for play button
     public void play (View view) {
-       Intent game_engine = new Intent(this, GameEngine.class);
-       // Take in input from all buttons - right now it just calls the next screen
+       Intent game_engine = new Intent(this, BoardView.class);
+
+       EditText editText = (EditText) findViewById(R.id.editText);
+       EditText editText2 = (EditText) findViewById(R.id.editText2);
+       Switch pvpSwitch = (Switch) findViewById(R.id.switch2);
+       Switch xGoesSwitch = (Switch) findViewById(R.id.switch3);
+       boolean playerVCPU = !pvpSwitch.isChecked();
+       boolean xGoesFirst = !xGoesSwitch.isChecked();
+       String message = editText.getText().toString();
+       String message2 = editText2.getText().toString();
+       game_engine.putExtra(PVPSWITCH, playerVCPU);
+       game_engine.putExtra(XOSWITCH, xGoesFirst);
+       game_engine.putExtra(XNAME, message);
+       game_engine.putExtra(ONAME, message2);
        startActivity(game_engine);
     }
 }
