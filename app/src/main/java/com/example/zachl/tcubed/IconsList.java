056cee4d0d0168c6fe019c71ac0df8b31b502842
Jillianm <jillian.e.mcdaniel@biola.edu>
2018-11-11 22:04:44 -0800

    Icons page done

--- app/src/main/java/com/example/zachl/tcubed/IconsList.java
+++ app/src/main/java/com/example/zachl/tcubed/IconsList.java
@@ -2,6 +2,7 @@ package com.example.zachl.tcubed;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class IconsList extends AppCompatActivity {
 
@@ -9,5 +10,55 @@ public class IconsList extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_icons_list);
+    }
+
+    // Intent for Icon 1
+    public void Icon1(View view) {
+
+    }
+
+    // Intent for Icon 2
+    public void Icon2(View view) {
+
+    }
+
+    // Intent for Icon 3
+    public void Icon3(View view) {
+
+    }
+
+    // Intent for Icon 4
+    public void Icon4(View view) {
+
+    }
+
+    // Intent for Icon 5
+    public void Icon5(View view) {
+
+    }
+
+    // Intent for Icon 6
+    public void Icon6(View view) {
+
+    }
+
+    // Intent for Icon 7
+    public void Icon7(View view) {
+
+    }
+
+    // Intent for Icon 8
+    public void Icon8(View view) {
+
+    }
+
+    // Intent for Icon 9
+    public void Icon9(View view) {
+
+    }
+
+    // Intent for Icon 10
+    public void Icon10(View view) {
+
     }
 }
